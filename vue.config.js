f7cd226ee1a55351f79e15a76dba9d56e8abeb8f
shjung <seunghyun.jung.84@gmail.com>
2023-04-06 13:27:26 +0900

    pages docs

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+});
+
+module.exports = {
+  // publicPath: "/dashboard_vue2/",
+  publicPath: process.env.NODE_ENV === "production" ? "/dashboard_vue2/" : "/",
+  outputDir: "docs",
+};
